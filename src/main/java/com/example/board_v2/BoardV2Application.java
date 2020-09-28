d64a97cab3e144f9e1b9ded5616838ff9f78158b
WooSungHwan <doqndnffo@gmail.com>
2020-09-28 11:01:28 +0900

    Initial commit

---
+++ src/main/java/com/example/board_v2/BoardV2Application.java
@@ -0,0 +1,14 @@
+package com.example.board_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardV2Application {
+
+	public static void main(String[] args) {
+		System.out.println(1);
+		SpringApplication.run(BoardV2Application.class, args);
+	}
+
+}
