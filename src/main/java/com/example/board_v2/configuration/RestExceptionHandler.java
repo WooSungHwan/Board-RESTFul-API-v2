5f5420f88c403ad83c543b03f25d80d1bd4e3b49
WooSungHwan <doqndnffo@gmail.com>
2020-10-02 00:41:51 +0900

    BindingResult 예외 처리
    1. @Validated 예외처리 시 message 영문 -> 개발자가 설정한 메시지로 나오도록
    2. 여러개 메시지 -> errorDetails에 셋팅되도록 수정.

--- src/main/java/com/example/board_v2/configuration/RestExceptionHandler.java
+++ src/main/java/com/example/board_v2/configuration/RestExceptionHandler.java
@@ -2,7 +2,10 @@ package com.example.board_v2.configuration;
 
 import com.example.board_v2.configuration.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,6 +14,8 @@ import org.springframework.web.server.UnsupportedMediaTypeStatusException;
 
 import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
@@ -66,10 +71,26 @@ public class RestExceptionHandler {
      */
     @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
     @ExceptionHandler(UnsupportedMediaTypeStatusException.class)
-    public ErrorResponse handlerUnsupportedMediaTypeStatusException(UnsupportedMediaTypeStatusException e, HttpServletRequest req) {
+    public ErrorResponse handlerUnsupportedMediaTypeStatusException(UnsupportedMediaTypeStatusException e,
+                                                                    HttpServletRequest req) {
         log.error("===================== Handler UnsupportedMediaTypeStatusException =====================");
         e.printStackTrace();
         return new ErrorResponse(HttpStatus.UNSUPPORTED_MEDIA_TYPE, e.getMessage(), null);
+    }
+
+    /**
+     * 메소드 파라미터 valid 불통
+     * @param e
+     * @param req
+     * @return
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorResponse handlerMethodArgumentNotValidException(MethodArgumentNotValidException e,
+                                                                HttpServletRequest req) {
+        log.error("===================== Handler MethodArgumentNotValidException =====================");
+        e.printStackTrace();
+        return getErrorResponseByBindingResult(e.getBindingResult(), HttpStatus.BAD_REQUEST, "유효하지 않은 값이 있습니다.");
     }
 
     /**
@@ -84,6 +105,17 @@ public class RestExceptionHandler {
         log.error("===================== Handler Exception =====================");
         e.printStackTrace();
         return new ErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage(), null);
+    }
+
+    private ErrorResponse getErrorResponseByBindingResult(BindingResult bindingResult,
+                                                          HttpStatus httpStatus,
+                                                          String message) {
+        //BindingResult 객체를 이용한 여러가지 validation 정보를 처리한다. -> 왜? -> 여러개의 validation이 걸릴 수 있으니까.
+        List<String> errorDetails = bindingResult.getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+        return new ErrorResponse(httpStatus, message, errorDetails);
     }
 
 }
